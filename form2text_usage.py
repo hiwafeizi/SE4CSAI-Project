ebb5dde59df71e6f2c7bfb4f73edcbf1f57973ac
hiwa2001 <hiwafhiwaf@GMAIL.COM>
2024-11-07 09:38:14 +0100

    test usage

--- form2text_usage.py
+++ form2text_usage.py
@@ -20,11 +20,46 @@ def generate_description(tokenizer, model, form_input, max_length=128):
     return description
 
 # Load the saved model and tokenizer
-tokenizer, model = load_model('form2description-model')
+tokenizer, model = load_model('best_model')
 
-# Example form input (as a single string)
-form_input = "US, Black, Dog, Adult, Female, Medium, 3"
+# List of test cases with expected descriptions
+test_data = [
+    ("US, Black, Dog, Adult, Female, Medium, 3", "This sweet girl lost her owner when he recently went to Heaven and she now is looking for her forever home. $200 www.all4animalsrescue.org"),
+    ("US, Other, Cat, Baby, Male, Medium, 1", "2 gray kittens, 1 male and 1 female 1 gray tabby female 1 medium length black/gray female 1 medium length black w/gray tips female"),
+    ("US, Unknown, Dog, Adult, Male, Small, 2", "Spanky is a 2yr.old cutie, he is 6 pounds and looking for a wonderful home.He is neutered, mico chipped ,heartworm tested and current."),
+    ("US, White_Cream, Dog, Adult, Female, Large, 3", "Please call my foster mom, Jody @ (352)316-5155 to find out how sweet I am.. she will be posting my story soon.. Thanks, Ginger"),
+    ("US, Other, Cat, Adult, Female, Medium, 1", "I'm Cammy, they are currently working on my bio. Please check back! If you are interested not call or email! Better yet come meet me!"),
+    ("US, Other, Cat, Adult, Male, Medium, 1", "I'm CJ, they are currently working on my bio. Please check back! If you are interested not call or email! Better yet come meet me!"),
+    ("US, Unknown, Dog, Adult, Male, Large, 3", "Sevier County Humane Society 865-453-7000 959 Gnatty Branch Rd. Sevierville Closed Mondays. Open Tuesday, Wednesday, Thursday 12 - 7 Friday, Saturday, Sunday 12 - 5"),
+    ("US, White_Cream, Dog, Baby, Male, Medium, 1", "Adoption fee is $67 and includes vaccinations, spay/neuter, heartworm test, and microchip.        Available for adoption, foster, or rescue.        1869 Ames Blvd  Marrero, LA 70072"),
+    ("US, Other, Cat, Adult, Female, Medium, 3", "Misty is a sweet four year old russian blue/lavender tabby. Loves to curl up in your lap. Very sweet. Good with dogs and cats."),
+    ("US, Black, Dog, Adult, Female, Large, 1", "Cookie - 4 year old Shepherd mix Female - over 60 lbs (ON DIET!!!) Great with cats, kids, people but would prefer a home without other dogs."),
+    ("US, Unknown, Dog, Adult, Male, Small, 1", "Very sweet dog who would love a great home! Jimmy Choo, Border Collie Mix has been shared from Shelter Exchange - http://www.shelterexchange.org."),
+    ("US, Unknown, Dog, Adult, Female, Medium, 1", "Very sweet and loving and would love a good home! Dopey, Labrador Retriever Mix has been shared from Shelter Exchange - http://www.shelterexchange.org."),
+    ("US, Other, Cat, Adult, Female, Large, 3", "Sandy enjoys playing with and spending time with dogs. Most of all, she love to snuggle and she chirps in her sleep! Best purr ever!"),
+    ("US, Unknown, Horse, Adult, Female, Large, 3", "To learn more about Sansa, visit https://horseandridermatch.com/2018/06/08/sansa-2015-palomino-quarter-horse-filly-with-the-appalachian-trainer-face-off/"),
+    ("US, Other, Cat, Baby, Male, Medium, 2", "I am a fun, playful, 12 week old handsome little man! Please visit me at Petsmart in Farmingdale or contact me at www.awaprescue.com"),
+    ("US, Unknown, Cat, Baby, Male, Medium, 3", "Cute little Tobias was found all alone wondering in the campgrounds. Boy was he hungry! He is safe now and looking for his forever home!"),
+    ("US, Unknown, Rat, Young, Male, Medium, 3", "Male hooded rat. Hand tame and sweet. Will do well alone or with a cage mate. Most ages and colors of rats available for adoption."),
+    ("US, Unknown, Rat, Young, Male, Medium, 3", "Male hooded rat. Hand tame and sweet. Will do well alone or with a cage mate. Most ages and colors of rats available for adoption."),
+    ("US, Other, Cat, Adult, Female, Medium, 3", "Cousin It (spayed female) 3 years - tabby w/ white DSH - stray Front declawed Adoption Fee: $60 Adoption fee includes spay/neuter, up-to-date vaccinations and microchipping."),
+    ("US, Black, Dog, Adult, Male, Large, 1", "Blue is friendly, outgoing, active and very smart. He is good with children. His family lost their home and can no longer care for him."),
+    ("US, Black, Chicken, Adult, Male, Large, 2", "Axel is a black rooster that is not aggressive around humans, and loves watermelon as his favorite treat. He is looking for his forever home."),
+    ("US, Other, Cat, Adult, Female, Small, 1", "FIV/FELV negative Meet Fluffy at pet Valu in Mullica Hil NJ Adopter will need to brush/comb frequently and do eye care from tearing"),
+    ("US, Black_White, Cat, Young, Female, Medium, 1", "Evette is a beautiful girl that is very friendly, playful and a cuddler. She is very laid back. Spayed, micro-chipped and current on shots."),
+    ("US, Brown_Chocolate, Cat, Adult, Female, Small, 2", "Mocha is a pretty sweet princess. Mocha loves to be petted and curl up in your lap. She gets along with other kitties and dogs."),
+]
 
-# Generate and print the description for the given form data
-description = generate_description(tokenizer, model, form_input)
-print(f"Generated Description: {description}")
+# Function to generate description for a list of inputs and compare with expected output
+def test_descriptions(tokenizer, model, test_data):
+    for form_input, expected_description in test_data:
+        # Generate description for each form input
+        generated_description = generate_description(tokenizer, model, form_input)
+        
+        # Print the generated and expected descriptions
+        print(f"Form Input: {form_input}")
+        print(f"Generated Description: {generated_description}")
+        print(f"Expected Description: {expected_description}")
+
+# Run the tests
+test_descriptions(tokenizer, model, test_data)
--- translate_usage.py
+++ translate_usage.py
@@ -1,17 +1,58 @@
-from transformers import T5Tokenizer, T5ForConditionalGeneration
+from transformers import MarianMTModel, MarianTokenizer
 
-# Step 1: Load the saved model and tokenizer
-model = T5ForConditionalGeneration.from_pretrained('translation-model')
-tokenizer = T5Tokenizer.from_pretrained('translation-model')
+# Step 1: Load the saved MarianMT model and tokenizer
+model = MarianMTModel.from_pretrained('epoch_1')
+tokenizer = MarianTokenizer.from_pretrained('epoch_1')
 
 # Step 2: Define a function for translating sentences
-def translate_sentence(model, tokenizer, sentence, max_length=50):
+def translate_sentence(model, tokenizer, sentence, max_length=150):
     inputs = tokenizer(sentence, return_tensors="pt")
     outputs = model.generate(inputs['input_ids'], max_length=max_length)
     translation = tokenizer.decode(outputs[0], skip_special_tokens=True)
     return translation
 
-# Step 3: Translate a new sentence
-sentence = "Where is the nearest train station?"
-translation = translate_sentence(model, tokenizer, sentence)
-print(f"Translation: {translation}")
+# Test cases
+test_cases = [
+    ("I'm CJ, they are currently working on my bio. Please check back! If you are interested not call or email! Better yet come meet me!",
+     "Ik ben CJ, ze werken momenteel aan mijn bio. Kom later nog eens terug! Als je geïnteresseerd bent, bel of e-mail niet! Nog beter, kom me ontmoeten!"),
+    
+    ("Sevier County Humane Society 865-453-7000 959 Gnatty Branch Rd. Sevierville Closed Mondays. Open Tuesday, Wednesday, Thursday 12 - 7 Friday, Saturday, Sunday 12 - 5",
+     "Sevier County Humane Society 865-453-7000 959 Gnatty Branch Rd. Sevierville Gesloten op maandag. Geopend dinsdag, woensdag, donderdag 12 - 7, vrijdag, zaterdag, zondag 12 - 5"),
+    
+    ("Adoption fee is $67 and includes vaccinations, spay/neuter, heartworm test, and microchip. Available for adoption, foster, or rescue. 1869 Ames Blvd Marrero, LA 70072",
+     "Adoptiekosten zijn $67 en omvatten vaccinaties, sterilisatie/castratie, hartwormtest en microchip. Beschikbaar voor adoptie, pleegzorg of redding. 1869 Ames Blvd Marrero, LA 70072"),
+    
+    ("Misty is a sweet four year old Russian blue/lavender tabby. Loves to curl up in your lap. Very sweet. Good with dogs and cats.",
+     "Misty is een lieve vierjarige Russische blauwe/lavendelkleurige tabby. Ze kruipt graag op schoot. Erg lief. Goed met honden en katten."),
+    
+    ("Cookie - 4 year old Shepherd mix Female - over 60 lbs (ON DIET!!!) Great with cats, kids, people but would prefer a home without other dogs.",
+     "Cookie - 4 jaar oude vrouwelijke herdermix - meer dan 60 lbs (OP DIEET!!!) Geweldig met katten, kinderen, mensen, maar zou liever een huis hebben zonder andere honden."),
+    
+    ("Very sweet dog who would love a great home! Jimmy Choo, Border Collie Mix has been shared from Shelter Exchange - http://www.shelterexchange.org.",
+     "Zeer lieve hond die graag een geweldig thuis zou willen hebben! Jimmy Choo, Border Collie Mix, is gedeeld van Shelter Exchange - http://www.shelterexchange.org."),
+    
+    ("Very sweet and loving and would love a good home! Dopey, Labrador Retriever Mix has been shared from Shelter Exchange - http://www.shelterexchange.org.",
+     "Zeer lief en aanhankelijk en zou graag een goed thuis willen! Dopey, Labrador Retriever Mix, is gedeeld van Shelter Exchange - http://www.shelterexchange.org."),
+    
+    ("Sandy enjoys playing with and spending time with dogs. Most of all, she loves to snuggle and she chirps in her sleep! Best purr ever!",
+     "Sandy houdt ervan om te spelen en tijd door te brengen met honden. Maar bovenal houdt ze ervan om te knuffelen en ze piept in haar slaap! Beste spin ooit!"),
+    
+    ("To learn more about Sansa, visit https://horseandridermatch.com/2018/06/08/sansa-2015-palomino-quarter-horse-filly-with-the-appalachian-trainer-face-off/",
+     "Om meer te weten te komen over Sansa, bezoek https://horseandridermatch.com/2018/06/08/sansa-2015-palomino-quarter-horse-filly-with-the-appalachian-trainer-face-off/"),
+    
+    ("I am a fun, playful, 12-week-old handsome little man! Please visit me at Petsmart in Farmingdale or contact me at www.awaprescue.com",
+     "Ik ben een leuke, speelse, 12 weken oude knappe kleine man! Bezoek me alsjeblieft bij Petsmart in Farmingdale of neem contact met me op via www.awaprescue.com"),
+    
+    ("Cute little Tobias was found all alone wandering in the campgrounds. Boy was he hungry! He is safe now and looking for his forever home!",
+     "Schattige kleine Tobias werd helemaal alleen gevonden terwijl hij rondzwierf op de camping. Hij had honger! Hij is nu veilig en op zoek naar zijn eeuwige thuis!"),
+    
+    ("Male hooded rat. Hand tame and sweet. Will do well alone or with a cage mate. Most ages and colors of rats available for adoption.",
+     "Mannelijke kaprat. Handtam en lief. Zal goed gedijen alleen of met een kooigenoot. De meeste leeftijden en kleuren ratten zijn beschikbaar voor adoptie.")
+]
+
+
+# Step 4: Test each case and print results
+for en_sentence, expected_nl_sentence in test_cases:
+    translation = translate_sentence(model, tokenizer, en_sentence)
+    print(f"English: {en_sentence}")
+    print(f"Generated Dutch Translation: {translation}")
