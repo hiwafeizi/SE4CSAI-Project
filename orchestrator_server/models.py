883f9b669a11dec61a5636f0b1a065aaf5c7e0ae
Hiwa <45601300+hiwafeizi@users.noreply.github.com>
2024-11-29 09:18:58 +0100

    db update

--- orchestrator_server/models.py
+++ orchestrator_server/models.py
@@ -1,6 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password_hash = db.Column(db.String(256), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 class PetDescriptionRequest(db.Model):
     __tablename__ = 'pet_description_requests'
